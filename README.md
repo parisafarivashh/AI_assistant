928379f272701c012a4a0eefdcea609e1728feec
parisa <parisafarivash@gmail.com>
2023-12-03 14:06:42 +0330

    Saved sessions in json file

--- README.md
+++ README.md
@@ -1,11 +1,116 @@
 # AI_assistant
 
-Simple agent with openai
+ # Simple agent with openai
 
-output script:
+OpenAI assistants are a way to interact with an LLM with persistent session data. 
+This means that if you call a pre-defined thread of messages and an assistant ID,
+the assistant will "remember" your messages and its replies.
+Assistants have "threads" which allow them to store and access message history. 
+An assistant and a thread can be "run" together, 
+which instructs the AI to work with a specific set of messages. 
+
+
+Now that we have created our assistant, thread, and message functions, 
+we need a way to store our sessions. 
+The solution:
+I was creating a JSON file stored in the working directory that stores your 
+sessions and assistant IDs. 
+This makes it easy to choose which assistant you want to talk to;
+you can also name them to help you remember who is who.
+
+
+When you want to chat with one of your assistants, 
+run the script, and you’ll be asked to choose an existing session from the 
+set that is printed to the console or to make a new assistant.
+Here is the function that saves your assistant’s data locally:
+```commandline
+    @staticmethod
+    def _save_session(
+            assistant_id,
+            thread_id,
+            user_name_input,
+            file_path='chat_sessions.json',
+    ):
+        """ This function saves your session data locally, so you can easily
+        retrieve it from the JSON file at any time.
+        """
+        if os.path.exists(file_path):
+            with open(file_path, 'r') as file:
+                data = json.load(file)
+
+        else:
+            data = {"sessions": {}}
+
+        next_session_number = str(len(data["sessions"]) + 1)
+
+        data["sessions"][next_session_number] = {
+            "AssistantID": assistant_id,
+            "ThreadID": thread_id,
+            "UserNameInput": user_name_input,
+        }
+
+        # Save data back to file
+        with open(file_path, 'w') as file:
+            json.dump(data, file, indent=4)
+   ```
+
+
+each time that you are chatting with the AI, and say “exit”, this function is 
+automatically called to scrape your messages from the cloud and save them into 
+a text file:
+```commandline
+
+    def _collect_message_history(self, thread_id, user_name_input):
+        """ This function downloads and writes your entire chat history
+        to a text file, so you can keep your own records.
+        """
+        messages = self.client.beta.threads.messages.list(thread_id)
+        message_dict = json.loads(messages.model_dump_json())
+
+        with open(f'{user_name_input}_message_log.txt', 'w') as message_log:
+            for message in reversed(message_dict['data']):
+                text_value = message['content'][0]['text']['value']
+
+                # Adding a prefix to distinguish between user and assistant messages
+                if message['role'] == 'assistant':
+                    prefix = f"{user_name_input}: "
+                else:  # Assuming any other role is the user
+                    prefix = "You: "
+
+                # Writing the prefixed message to the log
+                message_log.write(prefix + text_value + '\n')
+
+        return f"Messages saved to {user_name_input}_message_log.txt"
 ```
-Please type a name for this chat session: omadeus
-Created Session with omadeus, Assistant ID: asst_Fr7VIWXKw5mVQrOyiGROqYOH and Thread ID: thread_xeGWHdbbhhgRs18Oh521XAec
-Please type your question: whats your name?
-omadeus: Hey, I'm Omadeus. Nice to meet you! What's yours?
+
+We can put all of the puzzle pieces together. The create lets you 
+choose your session from your stored JSON file, 
+or you can opt to create a new session.
+To start, simply run the code and follow the instructions. 
+To properly end a session, type “exit”
+
+output
+```commandline
+Type 'new' to make a new assistant session. Press 'Enter' to choose an existing assistant session: new
+
+Please type a name for this chat: omadeus
+Created Session with omadeus, Assistant ID: asst_UkeZ7Lu8NuIIGoBBCYFev3yZ and Thread ID: thread_8BXqhSzJD9QK4CAyaw0cuJd1
+Please type your question: what is your name?
+omadeus: Hey there! My name is Omadeus. What's on your mind today?
+Please type your question: how are you?
+omadeus: I'm doing well, thanks for asking! How about you?
+
+```
+
+output2:
+
+```commandline
+Type 'new' to make a new assistant session. Press 'Enter' to choose an existing assistant session: 
+Available Sessions:
+Session 1: omadeus
+Enter the session number to load: 1
+Created Session with omadeus, Assistant ID: asst_UkeZ7Lu8NuIIGoBBCYFev3yZ and Thread ID: thread_8BXqhSzJD9QK4CAyaw0cuJd1
+Please type your question: whats your name
+omadeus: Hey, I'm still Omadeus. What's up? Time for a new topic, or are you still feeling bored?
+
 ```
--- assistant.py
+++ assistant.py
@@ -1,81 +1,193 @@
 import json
+import os
 import time
+from datetime import datetime
 
 from openai import OpenAI
 
 
-def setup_assistant(client, assistant_name):
-    """ This function creates a new assistant with the OpenAI Assistant API.
-    """
-    assistant = client.beta.assistants.create(
-        name=assistant_name,
-        instructions= f"""
-            You are a friend. Your name is {assistant_name}. You are having a 
-            vocal conversation with a user. You will never output any markdown 
-            or formatted text of any kind, and you will speak in a concise, 
-            highly conversational manner. You will adopt any persona that the 
-            user may ask of you.
-            """,
-        model="gpt-4-1106-preview",
-    )
-    # Create a thread
-    thread = client.beta.threads.create()
-    return assistant.id, thread.id
+class Agent:
 
+    def __init__(self, token):
+        self.client = OpenAI(api_key=token)
 
-def send_message(client, thread_id, task):
-    """ This function sends your message into the thread object,
-    which then gets passed to the AI.
-    """
-    thread_message = client.beta.threads.messages.create(
-        thread_id,
-        role="user",
-        content=task,
-    )
-    return thread_message
+    def _setup_assistant(self, assistant_name):
+        """ This function creates a new assistant with the OpenAI Assistant API
+        """
+        assistant = self.client.beta.assistants.create(
+            name=assistant_name,
+            instructions=f"""
+               You are a friend. Your name is {assistant_name}. You 
+               are having a vocal conversation with a user. 
+               You will never output any markdown or formatted text of any 
+               kind, and you will speak in a concise, highly conversational 
+               manner. You will adopt any persona that the user may ask of you.
+               """,
+            model="gpt-4-1106-preview",
+        )
+        # Create a thread
+        thread = self.client.beta.threads.create()
+        return assistant.id, thread.id
 
+    def _send_message(self, thread_id, task):
+        """ This function sends your message into the thread object,
+        which then gets passed to the AI.
+        """
+        thread_message = self.client.beta.threads.messages.create(
+            thread_id,
+            role="user",
+            content=task,
+        )
+        return thread_message
 
-def run_assistant(client, assistant_id, thread_id):
-    """ Runs the assistant with the given thread and assistant IDs.
-    """
-    run = client.beta.threads.runs.create(
-        thread_id=thread_id,
-        assistant_id=assistant_id
-    )
-
-    while run.status == "in_progress" or run.status == "queued":
-        time.sleep(1)
-        run = client.beta.threads.runs.retrieve(
+    def _run_assistant(self, assistant_id, thread_id):
+        """ Runs the assistant with the given thread and assistant IDs.
+        """
+        run = self.client.beta.threads.runs.create(
             thread_id=thread_id,
-            run_id=run.id
+            assistant_id=assistant_id
         )
 
-        if run.status == "completed":
-            return client.beta.threads.messages.list(
-                thread_id=thread_id
+        while run.status == "in_progress" or run.status == "queued":
+            time.sleep(1)
+            run = self.client.beta.threads.runs.retrieve(
+                thread_id=thread_id,
+                run_id=run.id
             )
 
+            if run.status == "completed":
+                messages = self.client.beta.threads.messages.list(
+                    thread_id=thread_id
+                )
+                return messages
 
-def main_loop():
-    client = OpenAI(api_key='<TOKEN>')
+    @staticmethod
+    def _save_session(
+            assistant_id,
+            thread_id,
+            user_name_input,
+            file_path='chat_sessions.json',
+    ):
+        """ This function saves your session data locally, so you can easily
+        retrieve it from the JSON file at any time.
+        """
+        if os.path.exists(file_path):
+            with open(file_path, 'r') as file:
+                data = json.load(file)
 
-    user_name_input = input("Please type a name for this chat session: ")
+        else:
+            data = {"sessions": {}}
 
-    IDS = setup_assistant(client, assistant_name=user_name_input)
-    assistant_id = IDS[0]
-    thread_id = IDS[1]
-    if assistant_id and thread_id:
-        print(f"Created Session with {user_name_input}, Assistant ID: {assistant_id} and Thread ID: {thread_id}")
-        while True:
-            user_message = input("Please type your question: ")
-            send_message(client, thread_id, user_message)
-            messages = run_assistant(client, assistant_id, thread_id)
-            message_dict = json.loads(messages.model_dump_json())
-            most_recent_message = message_dict['data'][0]
-            assistant_message = most_recent_message['content'][0]['text']['value']
-            print(f"{user_name_input}: {assistant_message}")
+        next_session_number = str(len(data["sessions"]) + 1)
+
+        data["sessions"][next_session_number] = {
+            "AssistantID": assistant_id,
+            "ThreadID": thread_id,
+            "UserNameInput": user_name_input,
+        }
+
+        # Save data back to file
+        with open(file_path, 'w') as file:
+            json.dump(data, file, indent=4)
+
+    @staticmethod
+    def _display_sessions(file_path='chat_sessions.json'):
+        """ This function shows your available sessions when you request it
+        """
+        if not os.path.exists(file_path):
+            print("No sessions available.")
+            return
+
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+
+        print("Available Sessions:")
+        for number, session in data["sessions"].items():
+            print(f"Session {number}: {session['UserNameInput']}")
+
+    @staticmethod
+    def _get_session_data(session_number, file_path='chat_sessions.json'):
+        """ This function retrieves the session that you choose.
+        """
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+
+        session = data["sessions"].get(session_number)
+        if session:
+            return session["AssistantID"], session["ThreadID"], session["UserNameInput"]
+        else:
+            print("Session not found.")
+            return None, None
+
+    def _collect_message_history(self, thread_id, user_name_input):
+        """ This function downloads and writes your entire chat history
+        to a text file, so you can keep your own records.
+        """
+        messages = self.client.beta.threads.messages.list(thread_id)
+        message_dict = json.loads(messages.model_dump_json())
+
+        with open(f'{user_name_input}_message_log.txt', 'w') as message_log:
+            for message in reversed(message_dict['data']):
+                text_value = message['content'][0]['text']['value']
+
+                # Adding a prefix to distinguish between user and assistant messages
+                if message['role'] == 'assistant':
+                    prefix = f"{user_name_input}: "
+                else:  # Assuming any other role is the user
+                    prefix = "You: "
+
+                # Writing the prefixed message to the log
+                message_log.write(prefix + text_value + '\n')
+
+        return f"Messages saved to {user_name_input}_message_log.txt"
+
+    def create(self):
+        user_choice = input(
+            "Type 'new' to make a new assistant session. "
+            "Press 'Enter' to choose an existing assistant session: "
+        )
+        if user_choice == 'new':
+            user_name_input = input("Please type a name for this chat: ")
+            IDS = self._setup_assistant(assistant_name=user_name_input)
+            self._save_session(IDS[0], IDS[1], user_name_input)
+
+            assistant_id = IDS[0]
+            thread_id = IDS[1]
+
+        else:
+            self._display_sessions()
+            session_number = input("Enter the session number to load: ")
+            assistant_id, thread_id, user_name_input = self._get_session_data(
+                session_number
+            )
+
+        if assistant_id and thread_id:
+            print(
+                f"Created Session with {user_name_input}, "
+                f"Assistant ID: {assistant_id} and Thread ID: {thread_id}"
+            )
+            first_iteration = True
+            while True:
+                user_message = input("Please type your question: ")
+
+                if first_iteration:
+                    current_time = datetime.now().strftime("%Y-%m-%d %H:%M")
+                    user_message = f"It is now {current_time}. {user_message}"
+                    first_iteration = False
+
+                if user_message.lower() == 'exit':
+                    print("Exiting the program.")
+                    self._collect_message_history(thread_id, user_name_input)
+                    break
+
+                self._send_message(thread_id, user_message)
+                messages = self._run_assistant(assistant_id, thread_id)
+                message_dict = json.loads(messages.model_dump_json())
+                most_recent_message = message_dict['data'][0]
+                assistant_message = most_recent_message['content'][0]['text']['value']
+                print(f"{user_name_input}: {assistant_message}")
 
 
 if __name__ == "__main__":
-    main_loop()
+    Agent(token='token').create()
 
